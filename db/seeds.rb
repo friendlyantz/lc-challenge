8c8f9717403565c018a11d3245a9573d0c62d4b4
Anton <panteleev23@gmail.com>
2021-03-04 12:37:37 +1100

    FIX: missing par-s and sytax fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ require 'csv'
 require 'json'
 
 filepath_lgas = File.join(__dir__,'./lc-files/lgas.csv')
-filepath_addresses = File.join(__dir__,'./lc-files/addresses.csv'
-filepath_properties = File.join(__dir__,'./lc-files/properties.csv'
-filepath_properties_json= File.join(__dir__,'./lc-files/properties.json'
+filepath_addresses = File.join(__dir__,'./lc-files/addresses.csv')
+filepath_properties = File.join(__dir__,'./lc-files/properties.csv')
+filepath_properties_json= File.join(__dir__,'./lc-files/properties.json')
 
 # ========== lgas ==============
 puts "========== lgas =============="
@@ -13,7 +13,7 @@ puts "========== lgas =============="
 CSV.foreach(filepath_lgas) do |row|
   puts "#{row[0]} | #{row[1]} | #{row[2]}"
   file = Lgasfile.new(code: row[0].to_i,name: row[1].to_s, long_name: row[2].to_s)
-  puts "#{file.name} created!"
+  file.save ? (puts "#{file.name} created!") : "failed to create"
 end
 puts "======= lgas complete ========"
 # ==============================
