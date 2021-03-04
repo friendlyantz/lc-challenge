3431cfde2742ebbae96ca77f135abfa0d59ce5b5
Anton <panteleev23@gmail.com>
2021-03-04 11:40:57 +1100

    ARCH: parsing method created for CSV + JSON

--- db/lc-files/parsing.rb
+++ db/lc-files/parsing.rb
@@ -1,8 +1,30 @@
 require 'csv'
+require 'json'
 
-filepath = 'lgas.csv'
+filepath_lgas = 'lgas.csv'
+filepath_addresses = 'addresses.csv'
+filepath_properties = 'properties.csv'
+filepath_properties_json= 'properties.json'
 
-CSV.foreach(filepath) do |row|
-  # Here, row is an array of columns
+# ========== lgas ==============
+CSV.foreach(filepath_lgas) do |row|
   puts "#{row[0]} | #{row[1]} | #{row[2]}"
 end
+# ==============================
+
+# ========= addresses ==========
+CSV.foreach(filepath_addresses) do |row|
+  puts "#{row[0]} | #{row[1]} | #{row[2]}"
+end
+# ==============================
+
+# ======== properties ==========
+CSV.foreach(filepath_properties) do |row|
+  puts "#{row[0]} | #{row[1]} | #{row[2]}"
+end
+# ==============================
+
+# ====== properties JSON =======
+properties_json = File.read(filepath_properties)
+properties = JSON.parse(properties_json)
+# ==============================
