117c17d196448c39e5d541f72123dd7e14047ede
Anton <panteleev23@gmail.com>
2021-03-04 12:32:40 +1100

    ARCH: Seed - lgas created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,21 @@
 require 'csv'
 require 'json'
 
-filepath_lgas = 'lc-files/lgas.csv'
-filepath_addresses = 'lc-files/addresses.csv'
-filepath_properties = 'lc-files/properties.csv'
-filepath_properties_json= 'lc-files/properties.json'
+filepath_lgas = File.join(__dir__,'./lc-files/lgas.csv')
+filepath_addresses = File.join(__dir__,'./lc-files/addresses.csv'
+filepath_properties = File.join(__dir__,'./lc-files/properties.csv'
+filepath_properties_json= File.join(__dir__,'./lc-files/properties.json'
 
 # ========== lgas ==============
 puts "========== lgas =============="
+# Lgasfile.destroy_all
+
 CSV.foreach(filepath_lgas) do |row|
   puts "#{row[0]} | #{row[1]} | #{row[2]}"
+  file = Lgasfile.new(code: row[0].to_i,name: row[1].to_s, long_name: row[2].to_s)
+  puts "#{file.name} created!"
 end
+puts "======= lgas complete ========"
 # ==============================
 
 # ========= addresses ==========
