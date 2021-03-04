df101fa0eefa5101449f0f253083d43d4af6b2fb
Anton <panteleev23@gmail.com>
2021-03-04 11:57:18 +1100

    ARCH: LGAS Model-Control-View setup

--- db/lc-files/parsing.rb
+++ db/lc-files/parsing.rb
@@ -7,24 +7,28 @@ filepath_properties = 'properties.csv'
 filepath_properties_json= 'properties.json'
 
 # ========== lgas ==============
+puts "========== lgas =============="
 CSV.foreach(filepath_lgas) do |row|
   puts "#{row[0]} | #{row[1]} | #{row[2]}"
 end
 # ==============================
 
 # ========= addresses ==========
-CSV.foreach(filepath_addresses) do |row|
-  puts "#{row[0]} | #{row[1]} | #{row[2]}"
-end
+# puts "========= addresses =========="
+# CSV.foreach(filepath_addresses) do |row|
+#   puts "#{row[0]} | #{row[1]} | #{row[2]} | #{row[3]} | #{row[4]} | #{row[5]}"
+# end
 # ==============================
 
 # ======== properties ==========
-CSV.foreach(filepath_properties) do |row|
-  puts "#{row[0]} | #{row[1]} | #{row[2]}"
-end
+# puts "======== properties =========="
+# CSV.foreach(filepath_properties) do |row|
+#   puts "#{row[0]} | #{row[1]} | #{row[2]} | #{row[3]} | #{row[4]}"
+# end
 # ==============================
 
 # ====== properties JSON =======
-properties_json = File.read(filepath_properties)
-properties = JSON.parse(properties_json)
+# puts "====== properties JSON ======="
+# properties_json = File.read(filepath_properties)
+# properties = JSON.parse(properties_json)
 # ==============================
